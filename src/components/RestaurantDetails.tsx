52ab6d126594aef0db8ec21a5dc655df79bc0094
Oliver Patrick Holroyd <opholroyd@gmail.com>
2024-09-23 13:10:41 +0100

    Missed response type definition on RestaurantList

--- src/components/RestaurantDetails.tsx
+++ src/components/RestaurantDetails.tsx
@@ -1,35 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Card, Container } from "react-bootstrap";
 import { getRestaurantDetails } from "../services/api";
+import { useQuery } from "@tanstack/react-query";
+import { RestaurantDetailsData } from "../types/RestaurantDetails";
+import Error from "./Error";
+import Loading from "./Loading";
 
 type RestaurantDetailsProps = {
   restaurantId: number;
 };
 
-type RestaurantDetailsData = {
-  address: string;
-  openingHours: {
-    weekday: string;
-    weekend: string;
-  };
-  reviewScore: number;
-  contactEmail: string;
-};
-
 const RestaurantDetails: React.FC<RestaurantDetailsProps> = ({
   restaurantId,
 }) => {
-  if (!restaurantId) return null;
+  const {
+    isPending,
+    error,
+    data: details,
+  } = useQuery<RestaurantDetailsData>({
+    queryKey: ["restaurant", restaurantId],
+    queryFn: () => getRestaurantDetails(restaurantId),
+    enabled: !!restaurantId,
+  });
 
-  const details = {
-    address: "123 Fine St, London",
-    openingHours: {
-      weekday: "12:00 PM - 10:00 PM",
-      weekend: "11:00 AM - 11:00 PM",
-    },
-    reviewScore: 4.7,
-    contactEmail: "info@velvetandvine.co.uk",
-  };
+  if (isPending) return <Loading />;
+  if (error) return <Error errorMessage={`Error: error.message`} />;
+
+  // const details = {
+  //   address: "123 Fine St, London",
+  //   openingHours: {
+  //     weekday: "12:00 PM - 10:00 PM",
+  //     weekend: "11:00 AM - 11:00 PM",
+  //   },
+  //   reviewScore: 4.7,
+  //   contactEmail: "info@velvetandvine.co.uk",
+  // };
 
   return (
     <Container>
